16014c604bd14d52d6254ea3177ad3bc0eeccd2d
ulleong-ulleong <blissfate03@gmail.com>
2023-10-30 16:45:07 +0900

    [ks48git] 초기 프로젝트 셋팅

---
+++ ks48git/src/main/java/ksmart/ks48git/Ks48gitApplication.java
@@ -0,0 +1,13 @@
+package ksmart.ks48git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ks48gitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ks48gitApplication.class, args);
+	}
+
+}
---
+++ ks48git/src/test/java/ksmart/ks48git/Ks48gitApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.ks48git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ks48gitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
